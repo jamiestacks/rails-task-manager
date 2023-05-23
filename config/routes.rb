a2655f9f4fb3e89c493f1f7f262f47fc630bfef6
Jamie Graham <jagraham04@yahoo.com>
2023-05-23 16:02:05 +0100

    Created working tasks list

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #read
+  get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  #create
+  get "task/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  #update
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+
+  #delete
+  delete "tasks/:id", to: "tasks#destroy"
 end
